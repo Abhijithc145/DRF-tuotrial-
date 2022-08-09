56b644be921e0e94f113293fe15cde0a6dbab0f6
Abhijith <abhijithabhipgmr145@gmail.com>
2022-08-09 11:20:07 +0530

    The function based view is using the serilizer , And Edit, Add  and Delete the data in the list . The status code and try  exceptions are using the Api views

--- whatchapp/serializers.py
+++ whatchapp/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from whatchapp.models import Movie
 from .models import *
 
@@ -13,3 +12,10 @@ class MovieSerilizer(serializers.Serializer):
 
     def create(self,validated_data):
         return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name",instance.name)
+        instance.discription = validated_data.get("discription",instance.discription)
+        instance.active = validated_data.get("active",instance.active)
+        instance.save()
+        return instance
--- whatchapp/views.py
+++ whatchapp/views.py
@@ -1,21 +1,24 @@
-from django.urls import is_valid_path
 from .models import *
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+
 
 # Create your views here.
 @api_view(['GET','POST'])
 def Movielist(request):
     if request.method == "GET":
         movie = Movie.objects.all()
-        serializer = MovieSerilizer(movie,many=True)
+        serializer = MovieSerilizer(movie,many = True)
         return Response(serializer.data)
 
 
     if request.method == "POST":
+        print("--------------------77777------------------------------")
         serializer = MovieSerilizer(data=request.data)
         if serializer.is_valid():
+            print("--------------------88888------------------------------")
             serializer.save()
             return Response(serializer.data)  
         else:
@@ -23,8 +26,28 @@ def Movielist(request):
             # noo  
 
 
-@api_view(('GET',))
+@api_view(('GET','PUT','DELETE'))
 def Movielists(request,pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerilizer(movie)
-    return Response(serializer.data)    
+    if request.method == "GET":
+        try:
+           movie = Movie.objects.get(pk=pk)
+           serializer = MovieSerilizer(movie)
+           return Response(serializer.data)  
+        except Movie.DoesNotExist:
+            return Response({'Error':'The list doesn\'t exist ' },status=status.HTTP_400_BAD_REQUEST)   
+
+
+    if request.method == "PUT":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerilizer(movie,data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)        
+
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(serializer.data,status=status.HTTP_204_NO_CONTENT)
+    
