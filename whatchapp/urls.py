e8a041ad3a39e4fcf8e2967c15f6e787ca061877
Abhijith <abhijithabhipgmr145@gmail.com>
2022-08-10 11:42:11 +0530

    More Changes in the urls The urls are conntets two and path(review/,ReviewList.as_view(),name='reviewlist'), are deleted in the urls

--- whatchapp/urls.py
+++ whatchapp/urls.py
@@ -10,8 +10,11 @@ urlpatterns = [
     path("stream/",Stream.as_view(),name='StreamPlatform'),
     path("stream/<int:pk>",Streams.as_view(),name='StreamPlatforms'),
 
+    path("stream/<int:pk>/review",Streams.as_view(),name='StreamPlatforms'),
+    path("stream/review/<int:pk>",ReviewLists.as_view(),name='reviewlists'),
+
+
     path("review/",ReviewList.as_view(),name='reviewlist'),
-    path("review/<int:pk>",ReviewLists.as_view(),name='reviewlists'),
 
 
 
